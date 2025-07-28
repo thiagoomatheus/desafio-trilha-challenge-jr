0121a9f25d1437fbe759785043a1f1cb9948f585
thiagoomatheus <thiagomatheus2001@hotmail.com>
2025-07-27 23:52:28 -0300

    adicionado html, css (básico com resposnividade) e script js para faq-section

---
+++ script/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menu = document.querySelector("#menu-icon");
+
+    const faq_lis = document.querySelectorAll("#faq-section ul li");
+    const faq_paragraphs = document.querySelectorAll("#faq-section ul li p");
+    const faq_icons = document.querySelectorAll("#faq-section ul li i");
+
+    faq_icons.forEach((icon, i) => {
+        icon.addEventListener("click", () => {
+            icon.classList.toggle("active");
+            faq_paragraphs[i].classList.toggle("active");
+            faq_lis[i].classList.toggle("active");
+        })
+    })
+
+    menu.addEventListener("click", () => {
+        const nav = document.querySelector("nav");
+        nav.classList.toggle("active");
+        menu.classList.toggle("active");
+    });
+
+
+})
